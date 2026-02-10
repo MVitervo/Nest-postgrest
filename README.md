740f42c59c644936e5874705838eb23efdda18c0
mvitervoIMSSA <miguel.vitervo@imssa1.com>
2026-02-10 11:06:39 -0700

    fin del video 121

--- README.md
+++ README.md
@@ -4,7 +4,20 @@
 
 # Teslo API
 
-1. Levantar la base de datos
+1. Clonar el proyecto
+
+2. ```npm install```
+
+3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+
+4. Cambiar las variables de entorno (puede ser del archivo .env)
+
+5. Levantar la base de datos
 ```
 docker-compose up -d
+```
+
+6. Ejecutar la aplicacion en modo desarrollo
+```
+npm run start:dev
 ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: []
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true, // carga en automatico las entidades que vamos definiendo poco a poco
+      synchronize: true // este no se suele pasar a prd ya que la funcion de esto es que si en dev borramos registros
+      // quitamos o agregramos columnas afectara inmediatamente a prd al subir los cambias con esta propiedad activada
+    })
+  ]
 })
 export class AppModule {}
