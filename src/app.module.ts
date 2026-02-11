785cf540c23434b978bd792b2a1508cde871ea91
mvitervoIMSSA <miguel.vitervo@imssa1.com>
2026-02-11 11:18:32 -0700

    fin de video 122 y 123

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -16,7 +17,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true, // carga en automatico las entidades que vamos definiendo poco a poco
       synchronize: true // este no se suele pasar a prd ya que la funcion de esto es que si en dev borramos registros
       // quitamos o agregramos columnas afectara inmediatamente a prd al subir los cambias con esta propiedad activada
-    })
+    }),
+
+    ProductsModule
   ]
 })
 export class AppModule {}
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,41 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column('text', {
+        unique: true // se indica que el titulo debe de ser unico
+    })
+    title: string
+
+    @Column('numeric', {
+        default: 0
+    })
+    price: number
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string
+
+    @Column('text', {
+        unique: true // se indica que el titulo debe de ser unico
+    })
+    slug: string
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[]
+
+    @Column('text')
+    gender: string
+}
