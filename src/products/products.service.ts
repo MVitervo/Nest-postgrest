f5fa1a1ace878ae2e33335d914e0c3bc62367814
mvitervoIMSSA <miguel.vitervo@imssa1.com>
2026-02-20 09:56:49 -0700

    fin del video 125

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+
+    try {
+
+      const product = this.productRepository.create(createProductDto); // creo el registro
+      await this.productRepository.save(product); // lo guarda en la base de datos
+
+      return product;
+
+    } catch (error) {
+
+      console.log(error);
+      throw new InternalServerErrorException('Help!');
+
+    }
+    
   }
 
   findAll() {
